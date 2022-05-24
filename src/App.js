df699f1a143c27f4f51ddbb81a4157311c28945d
Vladimir Kirpichev <djbobah@mail.ru>
2022-05-24 16:38:28 +0300

    start lesson 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { GlobalStyle } from "./components/GlobalStyle";
 import { NavBar } from "./components/NavBar";
 import { Menu } from "./components/Menu";
 
 function App() {
+  const [openItem, setOpenItem] = useState("");
+  console.log("item: ", openItem);
   return (
     <>
       <GlobalStyle />
       <NavBar />
-      <Menu />
+      <Menu setOpenItem={setOpenItem} />
     </>
   );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -39,11 +39,15 @@ const Item = styled.li`
   }
 `;
 
-const ListItem = ({ itemList }) => {
+const ListItem = ({ itemList, setOpenItem }) => {
   return (
     <List>
       {itemList.map((item) => (
-        <Item key={item.id} img={item.img}>
+        <Item
+          key={item.id}
+          img={item.img}
+          onClick={() => setOpenItem(item.name)}
+        >
           <p>{item.name}</p>
           <p>
             {item.price.toLocaleString("ru-RU", {
